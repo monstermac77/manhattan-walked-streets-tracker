0a146bb8a1242cb5ec3234be5173bd5b32ee69a6
Joe Puccio <josephpuccio@gmail.com>
2022-09-14 07:12:01 +0000

    switching to panda because they got this nice calculation thing

--- generateMap.py
+++ generateMap.py
@@ -4,19 +4,30 @@
 # sudo apt-get update
 # sudo apt-get install libgdal-dev
 # pip install fiona
+# pip install scipy
+# pip install geopandas
 
-import fiona
 import pprint
+import fiona
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+
+from scipy.spatial import cKDTree
+from shapely.geometry import Point
 
 # keep this handy: https://streets.planning.nyc.gov/about?lat=-73.9634&lng=40.7743&zoom=17
+# since most of what we'll be doing is finding distances to points, this will be most used and so lets switch to geopandas: https://gis.stackexchange.com/q/222315
 
+# once we're ready for the entire thing, we can do this instead. But it takes too long to do during development.
+# city = gpd.read_file("citymap_citymap_v1/citymap_citymap_v1.shp")
+
+# filtering to a smaller number during development
 city = fiona.open("citymap_citymap_v1/citymap_citymap_v1.shp")
-
-#first feature of the shapefile
 streets = []
 for i in range(0,1000):
 	element = city.next()
-	
+
 	# check if it's a street in Manhattan
 	elementType = element['properties']['type']
 	elementBorough = element['properties']['boro_nm']
@@ -24,4 +35,6 @@ for i in range(0,1000):
 	if elementType == "Mapped Street" and elementBorough == "Manhattan":
 		streets.append(element)
 
-pprint.pprint(streets)
+partialCity = gpd.GeoDataFrame.from_features(streets)
+print partialCity
+sys.exit(1)
